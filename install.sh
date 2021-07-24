64c63f208a587d91cb71d739583a0f12de0f26a1
Gabriel Caussi <gcaussi@gmail.com>
2021-07-24 12:12:39 -0300

    Add VS Code install to install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,6 @@
 ## Github:      https://github.com/gabrielcaussi
 ## Linkedin:    https://www.linkedin.com/in/gabrielcaussi
 
-echo ""
-echo ""
-echo " ___ _   _ ____ _____  _    _     _     _____ ____   "
-echo "|_ _| \ | / ___|_   _|/ \  | |   | |   | ____|  _ \  "
-echo " | ||  \| \___ \ | | / _ \ | |   | |   |  _| | |_) | "
-echo " | || |\  |___) || |/ ___ \| |___| |___| |___|  _ <  "
-echo "|___|_| \_|____/ |_/_/   \_\_____|_____|_____|_| \_\ "
-echo ""
-echo ""
-
 ## Update System
 sudo dnf update -y
 sudo dnf upgrade -y
@@ -56,79 +46,8 @@ if ! [ -x "$(command -v virtualbox)" ]; then
   rm VirtualBox-6.1-6.1.24_145767_fedora33-1.x86_64.rpm*
 fi
 
-## ====================================================================
-##      Configure Kitty
-## ====================================================================
-
-## Create directory ~/.config
-mkdir ~/.config
-
-## Symbolic link
-ln -sf ~/.dotfiles/.config/kitty/ ~/.config/
-
-## ====================================================================
-##      Configure Neovim
-## ====================================================================
-
-## Install Vim-plug
-curl -fLo ~/.dotfiles/.config/nvim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-## Symbolic link
-ln -sf ~/.dotfiles/.config/nvim/ ~/.config/
-
-## ====================================================================
-##      Configure Git
-## ====================================================================
-
-## Symbolic link
-ln -sf ~/.dotfiles/.gitconfig ~/
-
-## ====================================================================
-##      Configure Tmux
-## ====================================================================
-
-## Symbolic link
-ln -sf ~/.dotfiles/.tmux.conf ~/
-
-## ====================================================================
-##      Configure ZSH
-## ====================================================================
-
-## Install Oh-My-Zsh
-RUN_ZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-## Change default Shell to ZSH
-sudo chsh -s /bin/zsh
-
-## Symbolic link
-ln -sf ~/.dotfiles/.zshrc ~/
-
-## ====================================================================
-##      Configure Theme
-## ====================================================================
-
-## Symbolic link
-ln -sf ~/.dotfiles/.themes/ ~/
-
-## ====================================================================
-##      Configure Icons
-## ====================================================================
-
-## Symbolic link
-ln -sf ~/.dotfiles/.icons/ ~/
-
-## ====================================================================
-##      Configure Fonts
-## ====================================================================
-
-## Symbolic link
-ln -sf ~/.dotfiles/.fonts/ ~/
-
-## ====================================================================
-##      Configure GTK
-## ====================================================================
-
-## Symbolic link
-ln -sf ~/.dotfiles/.config/gtk-3.0/ ~/.config/
-ln -sf ~/.dotfiles/.gtkrc-2.0 ~/
+## install Visual Studio Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
+sudo dnf install code
