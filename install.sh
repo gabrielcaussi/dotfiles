62c5e7463c75a68b3fbd20da083b6a65e2e61292
Gabriel Caussi <gcaussi@gmail.com>
2021-10-20 23:02:58 -0300

    Migrate to Linux Mint

--- install.sh
+++ install.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-# Project     : Fedora Workstation
-# Description : DevOps Engineer Workstation Running Fedora Linux
+# Project     : Linux Mint Workstation
+# Description : Workstation Running Linux Mint
 # Developer   : Gabriel Caussi
 # Github      : https://github.com/gabrielcaussi
 # Linkedin    : https://www.linkedin.com/in/gabrielcaussi
 
 ## Update System
-sudo dnf update -y
-sudo dnf upgrade -y
-
-## Install fedora non-free repository
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf update -y
+sudo apt update -y
+sudo apt upgrade -y
 
 ## Install wifi module for Thinkpad E431 (BCM43142)
-sudo dnf install broadcom-wl -y
+sudo apt install broadcom-sta-dkms -y
 
 ## Install dependencies and softwares
-sudo dnf install util-linux-user xclip ffmpeg wget curl git neofetch htop zsh tmux neovim kitty hugo flameshot vagrant google-roboto-fonts -y
+sudo apt install util-linux xclip ffmpeg wget curl git neofetch htop zsh tmux neovim kitty hugo flameshot fonts-roboto fonts-jetbrains-mono -y
 
 ## Install Google Chrome
 if ! [ -x "$(command -v google-chrome)" ]; then
-  sudo dnf install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -y
-fi
-
-## Install Virtualbox
-if ! [ -x "$(command -v virtualbox)" ]; then
-  sudo dnf install https://download.virtualbox.org/virtualbox/6.1.24/VirtualBox-6.1-6.1.24_145767_fedora33-1.x86_64.rpm -y
+  sudo apt install https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -y
 fi
 
 ## Create directory ~/.config
@@ -56,12 +47,9 @@ ln -sf ~/dotfiles/.tmux.conf ~/
 ## Set themes, icons and cursors
 ln -sf ~/dotfiles/.themes/ ~/
 ln -sf ~/dotfiles/.icons/ ~/
-gsettings set org.gnome.desktop.interface gtk-theme "dracula-theme"
-gsettings set org.gnome.desktop.interface icon-theme "Papirus-Black-Dark"
-gsettings set org.gnome.desktop.interface cursor-theme "cursor-dracula"
-
-## Fonts
-ln -sf ~/dotfiles/.fonts/ ~/
+gsettings set org.gnome.cinnamon.interface gtk-theme "Flat-Remix-GTK-Teal-Dark"
+gsettings set org.gnome.cinnamon.interface icon-theme "Papirus-Dark"
+gsettings set org.gnome.cinnamon.interface cursor-theme "cursor"
 
 ## Oh-My-Zsh
 RUN_ZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
